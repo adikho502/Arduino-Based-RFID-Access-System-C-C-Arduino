90994141b86d0a3595e4734ab839ab0f09160593
adikho502 <adikhobusiness@gmail.com>
2025-09-20 14:08:11 -0400

    Add state machine functions to state_machine.ino

---
+++ Code/state_machine.ino
@@ -0,0 +1,71 @@
+void state1() {
+    // Implementation of state1()
+}
+
+void state2() {
+    // Implementation of state2()
+}
+
+void state3() {
+    // Implementation of state3()
+}
+
+void scan() {
+    // Implementation of scan()
+}
+
+void sensprox() {
+    // Implementation of sensprox()
+}
+
+void latchopen() {
+    // Implementation of latchopen()
+}
+
+void joystick() {
+    // Implementation of joystick()
+}
+
+void sensproxon() {
+    // Implementation of sensproxon()
+}
+
+void sensproxoff() {
+    // Implementation of sensproxoff()
+}
+
+void ledprox() {
+    // Implementation of ledprox()
+}
+
+void buzzprox() {
+    // Implementation of buzzprox()
+}
+
+void gaugeprox() {
+    // Implementation of gaugeprox()
+}
+
+void flashingLED() {
+    // Implementation of flashingLED()
+}
+
+void buzzmaxfreq() {
+    // Implementation of buzzmaxfreq()
+}
+
+void gaugeproxmax() {
+    // Implementation of gaugeproxmax()
+}
+
+void LEDoff() {
+    // Implementation of LEDoff()
+}
+
+void buzzeroff() {
+    // Implementation of buzzeroff()
+}
+
+void gaugeproxdefault() {
+    // Implementation of gaugeproxdefault()
+}
